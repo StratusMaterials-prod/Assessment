1245f8cb5318467b39379ef253f3e24cb3c87700
jiang <jiangc@stratusmaterials.com>
2024-10-22 13:32:56 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-"# Assessment" 
+
+# Data Science Assessment
+
+- read csv (mock_data.csv) and print the dataframe
+- plot bar chart on State, y is the number of customer
+- plot line chart, x is Purchate Date, y is the Purchase Amount
+- Create a new table call State_data with below columns
+
+  - State,
+  - number of customers,
+  - average age,
+  - total purchase amount
+
+# Python Assessment
+
+'''
+given a list of numbers, return the index of the number if found, else return -1
+'''
+
+'''
+write a function to return a list of fibonacci numbers, input is the length of list
+eg: fib(3) = [0,1,1]
+fib(6) = [0,1,1,2,3,5]
+'''
+
+'''
+given a list of numbers, return the index of the number if found, else return -1, use binary search
+'''
---
+++ python_assessment.py
@@ -0,0 +1,37 @@
+'''
+given a list of numbers, return the index of the number if found, else return -1
+eg: array = [1,3,5,6,7,8,10]
+find(array, 5) = 2
+find(array, 9) = -1
+'''
+def find(array, num):
+    pass
+
+
+'''
+write a function to return a list of fibonacci numbers, input is the length of list
+eg: fib(3) = [0,1,1]
+fib(6) = [0,1,1,2,3,5]
+'''
+
+def fib(n):
+    pass
+
+'''
+given a list of numbers, return the index of the number if found, else return -1. binary search
+eg: array = [1,3,5,6,7,8,10]
+find(array, 5) = 2
+find(array, 9) = -1
+'''
+def bin_find(array, num):
+    pass
+    
+
+if __name__ == '__main__':
+    array = [1,3,5,6,7,8,10]
+    # print(find(array, 5))
+    # print(find(array, 9))
+    # print(fib(6))
+    # print(bin_find(array, 5))
+    # print(bin_find(array, 9))
+
